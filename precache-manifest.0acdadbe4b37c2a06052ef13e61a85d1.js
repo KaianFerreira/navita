639539760da84b1ad2e0c1357a10762508e8cdeb
KaianFerreira <kaianfsantos@gmail.com>
2020-11-18 09:13:48 -0300

    deploy

---
+++ precache-manifest.0acdadbe4b37c2a06052ef13e61a85d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0364801ee4be477423",
+    "url": "/navita/css/app.bc9011ca.css"
+  },
+  {
+    "revision": "33d249f31ac696f3ed330162fca42daf",
+    "url": "/navita/index.html"
+  },
+  {
+    "revision": "4a0364801ee4be477423",
+    "url": "/navita/js/app.26777032.js"
+  },
+  {
+    "revision": "c45537693444f51b5fec",
+    "url": "/navita/js/chunk-vendors.a33acf89.js"
+  },
+  {
+    "revision": "cf2dcf3b7f5e73c4b9634a4b25a3a2c7",
+    "url": "/navita/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/navita/robots.txt"
+  }
+]);
